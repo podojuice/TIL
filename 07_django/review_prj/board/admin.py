1c4e663f4cd85d310692c21f9343c55458e89b29
Yeohyungoo <hyungu0611@gmail.com>
2019-04-08 15:00:19 +0900

    finish urls models admin

--- 07_django/review_prj/board/admin.py
+++ 07_django/review_prj/board/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
+
+admin.site.register(Article)
+
+
 
 # Register your models here.
+
+
--- 07_django/review_prj/board/models.py
+++ 07_django/review_prj/board/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+
+    def __str__(self):
+        return f'{self.id}: {self.title}'
+
+
 # Create your models here.
