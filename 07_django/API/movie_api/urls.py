79419b3f5cb41fc53431177c3fbf5ddce4c8ee8f
Yeohyungoo <hyungu0611@gmail.com>
2019-04-12 17:16:54 +0900

    0412

---
+++ 07_django/API/movie_api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from . import views
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_swagger_view(title='영화 api')
+
+urlpatterns = [
+    path('', schema_view),
+    path('movies/', views.movie_list, name='movie_list'),
+    path('movies/create/', views.create_movie, name='create_movie'),
+    path('movies/<int:movie_id>/', views.one_movie, name='one_movie'),
+
+
+]
