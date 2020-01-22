ffffb830c2fb5003af0c9fa9b414a44cd0ed3d54
hyungoo <hyungu0611@gmail.com>
2020-01-22 21:49:29 +0900

    memory view

--- FluentPython/2_tuple.py
+++ FluentPython/2_tuple.py
@@ -52,4 +52,63 @@ print(tokyo.population, tokyo.coordinates, tokyo[1])
 
 print(City._fields)
 
-# 불변 리스트로서의 튜플
+# 튜플은 불변 리스트로서의 역할도 어느정도 한다.
+
+# 슬라이싱
+
+l = list(range(10))
+
+print(l)
+
+l[2:5] = [20, 30]
+
+print(l)
+
+del l[5:7]
+
+print(l)
+
+l[3::2] = [11, 22]
+
+print(l)
+
+l[2:5] = [100]
+
+print(l)
+
+
+# 시퀀스의 복합 할당 ex) += , *=
+
+# 시퀀스에 던더 iadd가 구현돼있음녀 이게 나오는데, 없ㅇ느면 던더 add가 나옴. 그러니까 차이가 있음.
+
+# 가변 시퀀스일 경우 a의 값이 변경됨. 하지만, 뭐 그렇지 않을 수도 있따는 얘기.
+
+
+# 가변, 불변 시퀀스에 *=를 써보자.
+
+l = [1,2,3]
+
+print(l, id(l))
+
+l *=2
+
+print(l, id(l))
+
+t = (1,2,3)
+
+print(t, id(t))
+
+t*=2
+
+print(t, id(t))
+
+
+# list.sort() 와 sorted()
+
+# sort()메서드는 None을 반환. 이거는 API의 관례. 내부적으로 만들고 새 리스트를 만들지 않았다는얘기.
+
+# bisect 87p.
+
+# 메모리 뷰
+
+
